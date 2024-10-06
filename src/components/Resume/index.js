3e9b1229b9bc70a2d3263941b0292ffb286c3cc0
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-06 22:28:18 +0700

    Resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,6 +3,24 @@ import "./style.scss";
 import TimeLine from "../TimeLine";
 
 function Resume() {
+  const handleClick = (timeline) => {
+    const education = document.querySelector(".timeline-education");
+    const certificate = document.querySelector(".timeline-certificate");
+    const btnEducation = document.querySelector("[data-education]");
+    const btnCertificate = document.querySelector("[data-certificate]");
+    if (timeline === "education") {
+      btnEducation.classList.add("active");
+      btnCertificate.classList.remove("active");
+      education.style.display = "block";
+      certificate.style.display = "none";
+    } else if (timeline === "certificate") {
+      btnCertificate.classList.add("active");
+      btnEducation.classList.remove("active");
+      certificate.style.display = "block";
+      education.style.display = "none";
+    }
+  };
+
   return (
     <>
       <div className="resume">
@@ -11,10 +29,22 @@ function Resume() {
         <div className="bar">
           <Row gutter={20}>
             <Col xl={12} md={12} xs={24}>
-              <div className="box">Education</div>
+              <div
+                className="box"
+                data-education
+                onClick={() => handleClick("education")}
+              >
+                Education
+              </div>
             </Col>
             <Col xl={12} md={12} xs={24}>
-              <div className="box">Professional Skills</div>
+              <div
+                className="box"
+                data-certificate
+                onClick={() => handleClick("certificate")}
+              >
+                Certificate
+              </div>
             </Col>
           </Row>
         </div>
--- src/components/TimeLine/index.js
+++ src/components/TimeLine/index.js
@@ -5,126 +5,209 @@ function TimeLine() {
   return (
     <>
       <div className="timeline">
-        <Row gutter={50}>
-          <Col xxl={12} md={12} sm={24} xs={24}>
-            <div className="year">1998 - 2010</div>
-            <h1>Education Quality</h1>
-            <Timeline
-              items={[
-                {
-                  children: (
-                    <div className="box">
-                      <div className="title">First-Year Academic Results.</div>
-                      <div className="university">
-                        University Of Economy (2022-2023)
-                      </div>
-                      <div className="gpa">3.5/4.0</div>
+        <div className="timeline-education">
+          <Row gutter={50}>
+            <Col xxl={12} md={12} sm={24} xs={24}>
+              <div className="year">1998 - 2010</div>
+              <h1>Education Quality</h1>
+              <Timeline
+                items={[
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
 
-                      <p>
-                        The training provided by universities in order to
-                        prepare people to work in various sectors of the economy
-                        or areas of culture.
-                      </p>
-                    </div>
-                  ),
-                },
-                {
-                  children: (
-                    <div className="box">
-                      <div className="title">First-Year Academic Results.</div>
-                      <div className="university">
-                        University Of Economy (2022-2023)
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
                       </div>
-                      <div className="gpa">3.5/4.0</div>
+                    ),
+                  },
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
 
-                      <p>
-                        The training provided by universities in order to
-                        prepare people to work in various sectors of the economy
-                        or areas of culture.
-                      </p>
-                    </div>
-                  ),
-                },
-                {
-                  children: (
-                    <div className="box">
-                      <div className="title">First-Year Academic Results.</div>
-                      <div className="university">
-                        University Of Economy (2022-2023)
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
                       </div>
-                      <div className="gpa">3.5/4.0</div>
+                    ),
+                  },
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
 
-                      <p>
-                        The training provided by universities in order to
-                        prepare people to work in various sectors of the economy
-                        or areas of culture.
-                      </p>
-                    </div>
-                  ),
-                },
-              ]}
-            />
-          </Col>
-          <Col xxl={12} md={12} sm={24} xs={24}>
-            <div className="year">1998 - 2010</div>
-            <h1>Education Quality</h1>
-            <Timeline
-              items={[
-                {
-                  children: (
-                    <div className="box">
-                      <div className="title">First-Year Academic Results.</div>
-                      <div className="university">
-                        University Of Economy (2022-2023)
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
                       </div>
-                      <div className="gpa">3.5/4.0</div>
+                    ),
+                  },
+                ]}
+              />
+            </Col>
+            <Col xxl={12} md={12} sm={24} xs={24}>
+              <div className="year">1998 - 2010</div>
+              <h1>Education Quality</h1>
+              <Timeline
+                items={[
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
 
-                      <p>
-                        The training provided by universities in order to
-                        prepare people to work in various sectors of the economy
-                        or areas of culture.
-                      </p>
-                    </div>
-                  ),
-                },
-                {
-                  children: (
-                    <div className="box">
-                      <div className="title">First-Year Academic Results.</div>
-                      <div className="university">
-                        University Of Economy (2022-2023)
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
                       </div>
-                      <div className="gpa">3.5/4.0</div>
+                    ),
+                  },
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
 
-                      <p>
-                        The training provided by universities in order to
-                        prepare people to work in various sectors of the economy
-                        or areas of culture.
-                      </p>
-                    </div>
-                  ),
-                },
-                {
-                  children: (
-                    <div className="box">
-                      <div className="title">First-Year Academic Results.</div>
-                      <div className="university">
-                        University Of Economy (2022-2023)
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
                       </div>
-                      <div className="gpa">3.5/4.0</div>
+                    ),
+                  },
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
 
-                      <p>
-                        The training provided by universities in order to
-                        prepare people to work in various sectors of the economy
-                        or areas of culture.
-                      </p>
-                    </div>
-                  ),
-                },
-              ]}
-            />
-          </Col>
-        </Row>
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
+                      </div>
+                    ),
+                  },
+                ]}
+              />
+            </Col>
+          </Row>
+        </div>
+        <div className="timeline-certificate">
+          <Row gutter={50}>
+            <Col xxl={24} md={24} sm={24} xs={24}>
+              <div className="year">1998 - 2010</div>
+              <h1>Certificate</h1>
+              <Timeline
+                items={[
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
+
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
+                      </div>
+                    ),
+                  },
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
+
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
+                      </div>
+                    ),
+                  },
+                  {
+                    children: (
+                      <div className="box">
+                        <div className="title">
+                          First-Year Academic Results.
+                        </div>
+                        <div className="university">
+                          University Of Economy (2022-2023)
+                        </div>
+                        <div className="gpa">3.5/4.0</div>
+
+                        <p>
+                          The training provided by universities in order to
+                          prepare people to work in various sectors of the
+                          economy or areas of culture.
+                        </p>
+                      </div>
+                    ),
+                  },
+                ]}
+              />
+            </Col>
+          </Row>
+        </div>
       </div>
     </>
   );
