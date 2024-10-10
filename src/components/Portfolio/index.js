cc31dc4d6e208a22a3be1cd3624370648a5dddb6
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-10 13:47:14 +0700

    Hiệu ứng cho Portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,175 +7,57 @@ function Portfolio() {
   return (
     <>
       <div
-        className="portfolio  wow animate__animated animate__fadeIn"
+        className="portfolio wow animate__animated animate__fadeIn"
         data-wow-delay="0.5s"
         id="project"
       >
-        <h3>Visit my project and keep your feedback</h3>
-        <h1>My Project</h1>
+        <h3
+          className="wow animate__animated animate__fadeInDown"
+          data-wow-delay="0.3s"
+        >
+          Visit my project and keep your feedback
+        </h3>
+        <h1
+          className="wow animate__animated animate__fadeInUp"
+          data-wow-delay="0.4s"
+        >
+          My Project
+        </h1>
         <Row className="container" gutter={[30, 30]}>
-          <Col xl={8} md={12} sm={24} xs={24}>
-            <div className="box">
-              <div className="wrap-image">
-                <img
-                  src="https://rainbowit.net/themes/inbio/wp-content/uploads/2021/08/portfolio-large-01-340x250.jpg"
-                  alt=""
-                ></img>
-              </div>
-              <div className="wrap-content">
-                <div className="wrap-love">
-                  <div className="love">
-                    <div className="number-love">612</div>
-                    <span>
-                      {" "}
-                      <CiHeart />
-                    </span>
-                  </div>
+          {Array.from({ length: 6 }).map((_, index) => (
+            <Col xl={8} md={12} sm={24} xs={24} key={index}>
+              <div
+                className="box wow animate__animated animate__zoomIn"
+                data-wow-delay={`${0.6 + index * 0.2}s`}
+              >
+                <div className="wrap-image">
+                  <img
+                    src="https://rainbowit.net/themes/inbio/wp-content/uploads/2021/08/portfolio-large-01-340x250.jpg"
+                    alt=""
+                  />
                 </div>
-                <h2>
-                  NFT Dashboard Application Development.{" "}
-                  <span>
-                    <FaArrowUp />
-                  </span>{" "}
-                </h2>
-              </div>
-            </div>
-          </Col>
-          <Col xl={8} md={12} sm={24} xs={24}>
-            <div className="box">
-              <div className="wrap-image">
-                <img
-                  src="https://rainbowit.net/themes/inbio/wp-content/uploads/2021/08/portfolio-large-01-340x250.jpg"
-                  alt=""
-                ></img>
-              </div>
-              <div className="wrap-content">
-                <div className="wrap-love">
-                  <div className="love">
-                    <div className="number-love">612</div>
-                    <span>
-                      {" "}
-                      <CiHeart />
-                    </span>
+                <div className="wrap-content">
+                  <div className="wrap-love">
+                    <div
+                      className="love wow animate__animated animate__heartBeat"
+                      data-wow-delay={`${0.8 + index * 0.2}s`}
+                    >
+                      <div className="number-love">612</div>
+                      <span>
+                        <CiHeart />
+                      </span>
+                    </div>
                   </div>
-                </div>
-                <h2>
-                  NFT Dashboard Application Development.{" "}
-                  <span>
-                    <FaArrowUp />
-                  </span>{" "}
-                </h2>
-              </div>
-            </div>
-          </Col>
-          <Col xl={8} md={12} sm={24} xs={24}>
-            <div className="box">
-              <div className="wrap-image">
-                <img
-                  src="https://rainbowit.net/themes/inbio/wp-content/uploads/2021/08/portfolio-large-01-340x250.jpg"
-                  alt=""
-                ></img>
-              </div>
-              <div className="wrap-content">
-                <div className="wrap-love">
-                  <div className="love">
-                    <div className="number-love">612</div>
+                  <h2>
+                    NFT Dashboard Application Development{" "}
                     <span>
-                      {" "}
-                      <CiHeart />
+                      <FaArrowUp />
                     </span>
-                  </div>
+                  </h2>
                 </div>
-                <h2>
-                  NFT Dashboard Application Development.{" "}
-                  <span>
-                    <FaArrowUp />
-                  </span>{" "}
-                </h2>
               </div>
-            </div>
-          </Col>
-          <Col xl={8} md={12} sm={24} xs={24}>
-            <div className="box">
-              <div className="wrap-image">
-                <img
-                  src="https://rainbowit.net/themes/inbio/wp-content/uploads/2021/08/portfolio-large-01-340x250.jpg"
-                  alt=""
-                ></img>
-              </div>
-              <div className="wrap-content">
-                <div className="wrap-love">
-                  <div className="love">
-                    <div className="number-love">612</div>
-                    <span>
-                      {" "}
-                      <CiHeart />
-                    </span>
-                  </div>
-                </div>
-                <h2>
-                  NFT Dashboard Application Development.{" "}
-                  <span>
-                    <FaArrowUp />
-                  </span>{" "}
-                </h2>
-              </div>
-            </div>
-          </Col>
-          <Col xl={8} md={12} sm={24} xs={24}>
-            <div className="box">
-              <div className="wrap-image">
-                <img
-                  src="https://rainbowit.net/themes/inbio/wp-content/uploads/2021/08/portfolio-large-01-340x250.jpg"
-                  alt=""
-                ></img>
-              </div>
-              <div className="wrap-content">
-                <div className="wrap-love">
-                  <div className="love">
-                    <div className="number-love">612</div>
-                    <span>
-                      {" "}
-                      <CiHeart />
-                    </span>
-                  </div>
-                </div>
-                <h2>
-                  NFT Dashboard Application Development.{" "}
-                  <span>
-                    <FaArrowUp />
-                  </span>{" "}
-                </h2>
-              </div>
-            </div>
-          </Col>
-          <Col xl={8} md={12} sm={24} xs={24}>
-            <div className="box">
-              <div className="wrap-image">
-                <img
-                  src="https://rainbowit.net/themes/inbio/wp-content/uploads/2021/08/portfolio-large-01-340x250.jpg"
-                  alt=""
-                ></img>
-              </div>
-              <div className="wrap-content">
-                <div className="wrap-love">
-                  <div className="love">
-                    <div className="number-love">612</div>
-                    <span>
-                      {" "}
-                      <CiHeart />
-                    </span>
-                  </div>
-                </div>
-                <h2>
-                  NFT Dashboard Application Development.{" "}
-                  <span>
-                    <FaArrowUp />
-                  </span>{" "}
-                </h2>
-              </div>
-            </div>
-          </Col>
+            </Col>
+          ))}
         </Row>
       </div>
     </>
