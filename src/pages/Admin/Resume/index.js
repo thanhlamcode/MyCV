a9334f6583d513cb860f4e81fe9b97e403a988bd
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-12 13:34:19 +0700

    Giao diện sơ bộ trang quản trị

---
+++ src/pages/Admin/Resume/index.js
@@ -0,0 +1,8 @@
+function ResumeAdmin() {
+  return (
+    <>
+      <h1>Đây là trang resumeIntro</h1>
+    </>
+  );
+}
+export default ResumeAdmin;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,20 @@
+import LayoutAdmin from "../layout/LayoutAdmin";
+import ResumeAdmin from "../pages/Admin/Resume";
 import MainCV from "../pages/MainCV";
 
 export const routes = [
   {
     path: "/",
     element: <MainCV />,
+  },
+  {
+    path: "/admin",
+    element: <LayoutAdmin />,
+    children: [
+      {
+        path: "resume",
+        element: <ResumeAdmin />,
+      },
+    ],
   },
 ];
