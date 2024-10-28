1cfe8b0e1c5a7b38bb79ee7a4ef211a484a66f0f
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-28 23:23:52 +0700

    Tính năng log-out

--- src/components/Admin/Header/index.js
+++ src/components/Admin/Header/index.js
@@ -1,10 +1,29 @@
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { Header } from "antd/es/layout/layout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HeaderAdmin(props) {
   const { collapsed, setCollapsed, colorBgContainer } = props;
+  const history = useNavigate();
+
+  const handleRemoveToken = () => {
+    localStorage.removeItem("token");
+    history("/");
+  };
+
+  const handleLogOut = () => {
+    Modal.confirm({
+      title: "Bạn có chắc chắn muốn đăng xuất?",
+      onOk: handleRemoveToken,
+      footer: (_, { OkBtn, CancelBtn }) => (
+        <>
+          <CancelBtn />
+          <OkBtn onClick={handleRemoveToken} />
+        </>
+      ),
+    });
+  };
 
   return (
     <>
@@ -25,10 +44,21 @@ function HeaderAdmin(props) {
           }}
         />
         <Link to="/detail/thanhlam">
-          <Button style={{ position: "absolute", top: "20px", right: "10px" }}>
+          <Button style={{ position: "absolute", top: "20px", right: "110px" }}>
             Xem trang của bạn
           </Button>
         </Link>
+        <Button style={{ position: "absolute", top: "20px", right: "10px" }}>
+          Đăng xuất
+        </Button>
+        <Button
+          onClick={() => {
+            handleLogOut();
+          }}
+          style={{ position: "absolute", top: "20px", right: "10px" }}
+        >
+          Đăng xuất
+        </Button>
       </Header>
     </>
   );
